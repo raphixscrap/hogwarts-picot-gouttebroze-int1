bd316cfd684f2d292e4f44f82bec7a8493af8d75
Guimauve39631 <lazare.gouttebroze@gmail.com>
2025-11-28 16:20:28 +0100

    feat: add the function init_character and display_character to character.py

--- universe/character.py
+++ universe/character.py
@@ -1,1 +1,17 @@
-. 
+
+
+def init_character(last_name :str, first_name :str, attributes :dict)->dict:
+    return {"Last Name": last_name, "First Name": first_name, "Money": 100, "Inventory": [], "Attributes":attributes}
+
+def display_character (character : dict)->None:
+    print("Character profile: ")
+    for key,item in character.items():
+        if(type(item) == list):
+            print("{}: {}".format(key," ; ".join(item)))
+        elif(type(item)==dict):
+            print("{} :".format(key))
+            for item_key, item_item in item.keys():
+                print(" - {}: {}".format(item_key,item_item))
+        else:
+            print("{}: {}".format(key,item))
+    return
