0a51e6f677fa2b423ccc5ce821af63c0e8646033
Guimauve39631 <lazare.gouttebroze@gmail.com>
2025-11-28 15:09:17 +0100

    refactor: optimization of the function ask_number

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -18,38 +18,18 @@ def ask_number(message: str, min_val=None, max_val=None) -> int:
     :return: type-> int, the returned value is the number chosen by the user.
     """
     value = ask_text(message+" : ")
-    int_value = 0
-    digits = "1234567890"
-    negative = False
-    if (value[0] == "-"):
-        negative = True
-        value = value[1:]
-    for i in range (len(value)):
-        if(value[i] in digits):
-            int_value += (ord(value[i])-ord('0'))*10**(len(value)-i-1)
-        else :
-            if(min_val is None and max_val is None):
-                print("Please enter a number.")
-            elif(min_val is None and not max_val is None):
-                print("Please enter a number lesser or equal to {}.".format(max_val))
-            elif(not min_val is None and max_val is None):
-                print("Please enter a number greater or equal to {}.".format(min_val))
-            else:
-                print("Please enter a number between {} and {}.".format(min_val,max_val))
-            return ask_number(message,min_val,max_val)
-    if (negative):
-        int_value *= -1
-    if(min_val is None and max_val is None):
-        return int_value
-    elif(min_val is None and max_val<int_value):
-        print("Please enter a number lesser or equal to {}.".format(max_val))
-        return ask_number(message, min_val, max_val)
-    elif(max_val is None and min_val>int_value):
-        print("Please enter a number greater or equal to {}.".format(min_val))
-        return ask_number(message, min_val, max_val)
-    elif (not min_val<=int_value<=max_val):
-        print("Please enter a number between {} and {}.".format(min_val,max_val))
-        return ask_number(message, min_val, max_val)
+    try:
+        int_value=int(value)
+        if (min_val is None and max_val is None):
+            return int_value
+        elif (min_val is None and max_val < int_value):
+            raise ValueError
+        elif (max_val is None and min_val > int_value):
+            raise ValueError
+        elif (not min_val <= int_value <= max_val):
+            raise ValueError
+    except:
+        return ask_number(message,min_val,max_val)
     return int_value
 
 def ask_choice(message :str, options :list)->int:
