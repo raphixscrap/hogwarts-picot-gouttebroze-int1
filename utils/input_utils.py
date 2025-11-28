be9c9feb29714ff39b203a30d9d1a40835ccf7ea
Guimauve39631 <lazare.gouttebroze@gmail.com>
2025-11-28 09:19:41 +0100

    remove: .idea

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,4 +1,4 @@
-def ask_test(message: str) -> str:
+def ask_text(message: str)->str:
     value = ""
     while value.strip() == "":
         value = input(message)
@@ -9,14 +9,14 @@ def ask_number(message: str, min_val=None, max_val=None) -> int:
     negative = False
     accept_digits = "0123456789"
     value = None
-    while value == None or value < min_val or value > max_val:
+    while value is None or value < min_val or value > max_val:
         value_input=input(message)
-        for i in range(len(message)):
-           if(i == 0 and message[i] == "-"):
+        for i in range(len(value_input)):
+           if(i == 0 and value_input[i] == "-"):
                negative = True
            else:
-               if(message[i] in accept_digits):
-                   print("TEST")
+               if(not value_input[i] in accept_digits):
+
                else:
                    break
     return value
