0db7054b08705ceaa15ed7a34a43b37cce6f45e3
Raphix <picot.raphael@gmail.com>
2025-11-29 18:54:50 +0100

    Adding House Functions and finalizing characters functions

--- universe/character.py
+++ universe/character.py
@@ -1,7 +1,5 @@
-
-
 def init_character(last_name :str, first_name :str, attributes :dict)->dict:
-    return {"Last Name": last_name, "First Name": first_name, "Money": 100, "Inventory": [], "Attributes":attributes}
+    return {"Last Name": last_name, "First Name": first_name, "Money": 100, "Inventory": [], "Spells": [], "Attributes":attributes}
 
 def display_character (character : dict)->None:
     print("Character profile: ")
@@ -10,8 +8,24 @@ def display_character (character : dict)->None:
             print("{}: {}".format(key," ; ".join(item)))
         elif(type(item)==dict):
             print("{} :".format(key))
-            for item_key, item_item in item.keys():
+            for item_key, item_item in item.items():
                 print(" - {}: {}".format(item_key,item_item))
         else:
             print("{}: {}".format(key,item))
     return
+
+def get_full_name(character:dict)->str:
+    return character["First Name"] + character["Last Name"]
+
+
+def modify_money(character : dict, money :int)->dict:
+    character["Money"] += money
+    return character
+
+def add_item(character:dict, key:str, item:str)->dict:
+    if(key != "Inventory" and key != "Spells"):
+        print(f"[WARN] - CHARACTER_ADD_ITEM - Key is invalid for adding an item for {get_full_name(character)}")
+    else:
+        character[key].append(item)
+    return character
+
--- universe/house.py
+++ universe/house.py
@@ -1,1 +1,32 @@
-. 
+def update_house_points(houses:dict, house_name:str, points:int)->None:
+    if not house_name in houses:
+        print(f"[WARN] - HOUSE_UPDATE_POINTS - The House '{house_name}' don't exist")
+    else:
+        houses[house_name] += points
+    return
+
+def display_winning_house(houses:dict)->None:
+    max_value = None
+    winners = []
+    for name, value in houses.items():
+        if max_value is None:
+            max_value = value
+            winners.append(name)
+        else:
+            if max_value < value or max_value is None:
+                winners.clear()
+                max_value = value
+                winners.append(name)
+            elif max_value == value:
+                winners.append(name)
+
+    if len(winners) == 0:
+        print("[WARN] - HOUSE_DISPLAY_WINNING_HOUSE - House list empty")
+    elif len(winners) == 1:
+        print(f"The house with the highest score is {winners[0]} with {max_value} points.")
+    else:
+        print(f"The houses with the highest score are : {', '.join(winners)} with {max_value} points.")
+
+def assign_house(character:dict, questions:list[tuple[str, list[str], list[str]]])->None:
+    pass
+
