1e4e4766c3bea641b0aeaf1395c4bbce66fd6360
Guimauve39631 <lazare.gouttebroze@gmail.com>
2026-01-03 11:28:46 +0100

    feat: add function tutorial_fight and starting to implement the fight loop

--- hogwarts/chapters/chapter_5.py
+++ hogwarts/chapters/chapter_5.py
@@ -1,4 +1,5 @@
-from hogwarts.utils.input_utils import wait_pause
+from hogwarts.utils.input_utils import wait_pause, ask_choice
+
 
 def explanation ():
     print("Duels are turn based fights.")
@@ -15,11 +16,25 @@ def explanation ():
     print('You win a duel if your enemy\'s life reach 0 and yours is superior to 0. And vice versa.')
     wait_pause()
 
+def fight(character_duel, enemy_duel):
+    while(character_duel['HP']!=0 and enemy_duel["HP"]!=0):
+        turn(character_duel,enemy_duel)
+    if(character_duel['HP']!=0):
+        print("YOU WON THIS DUEL !")
+    else:
+        print("YOU LOST THIS DUEL !")
+
+def tutorial_fight(character:dict):
+    training_enemy = {"name":"Training bot","HP":20,'Attack':[2,4]}
+    character_duel = {"Name":character["First Name"],"HP":20,"Attack":[character["Attributes"]["Ambition"]%2,character["Attributes"]["Intelligence"]]}
+    fight(character_duel,training_enemy)
 
 def tutorial (character:dict):
     print("This tutorial is here to explain how to play a duel in our game.")
     wait_pause()
     explanation()
+    while( ask_choice("Do you want to make a first duel to understand how it works?",["yes","No"])==1):
+        tutorial_fight(character)
 
 
 
