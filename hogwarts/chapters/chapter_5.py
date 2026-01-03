3bc239c58a09d8f5e2dfcab2fd34a3e3a93aef56
Guimauve39631 <lazare.gouttebroze@gmail.com>
2026-01-03 10:26:26 +0100

    feat: creation of chapter_5.py

---
+++ hogwarts/chapters/chapter_5.py
@@ -0,0 +1,8 @@
+from hogwarts.utils.input_utils import wait_pause
+
+def start_chapter_5(character :dict):
+    print("=========== CHAPTER 5 : Duel against Voldemort during the Goblet of Fire ===========")
+    tutorial(character)
+    sceanario(character)
+    duel_voldemort(character)
+
